1e2990638c788ad6275969b4d721187271eb7ee2
anninganan <161664839+anninganan@users.noreply.github.com>
2024-03-11 12:28:50 +1300

    Add files via upload

--- quiz.py
+++ quiz.py
@@ -1,5 +1,5 @@
 score=0
-Dictionary = {"Gerry Austin steals a wallet and uses the cash to rent a Mini. Which is the colour of MIni?     A white  B yellow C black D red":"B",
+Dictionary = {"Gerry Austin steals a wallet and uses the cash to rent a Mini. Which is the colour of MIni?     A) white  B) yellow C) black D) red":"B",
               "Gerry and John pick up___,who is heading to Wanganui A Sue B Shirl  C Mulvaney  D YOU":"B",
               "Who helps the trios in wellington?   A Sue B Shirl  C Mulvaney D YOU":"C",
               "Who is the director of this movie(Goodbye Pork Pie)?   A Geoff Murphy  B John coker  C Tony Barry  D YOU":"A",
@@ -7,12 +7,23 @@ Dictionary = {"Gerry Austin steals a wallet and uses the cash to rent a Mini. Wh
 for x in Dictionary:
     print(x)
     answer=input("Your answer is  :")
-    if answer==Dictionary[x]:
-        print("correct")
-        score+=1
-    elif answer!=["A","B","C","D"]:
-        print("invaild NSWER, answer should in ABCD")
-        
+    validate_answers=['A','B','C','D']
+    while answer  not in validate_answers:
+        print("invalid answer answer should in A B C D")
+        answer=input("your answer is :")
+        print(validate_answers)
     else:
-        print("uncerrect")
+        print("your answer is"+ answer)
+        if answer==Dictionary[x]:
+            print("correct")
+            score+=1
+            print("your score is: ",score)
+        else:
+            print("Incorrect,the answer is",Dictionary[x])
+if score==[3,4] :
+    print("well done!but still need learn about it.")
+if score==[1,2] :
+    print("you need watch more about it")
+if score== 5 :
+    print("awsome,you pass this quiz")
         
