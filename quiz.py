9218e85412dcfbda80fac3a4e514488a6fd4a03f
anninganan <161664839+anninganan@users.noreply.github.com>
2024-03-20 10:09:38 +1300

    Add files via upload

--- quiz.py
+++ quiz.py
@@ -1,11 +1,20 @@
+
+#creat a list about username,use input function to let user input their name to login
+
 username=["alex","phonix","rio","sanhand","xiamrun","steve"]
 login=input("Enter your username: ").lower().strip()
+
+#use while loop to check the username is valid or invalid.
 while login not in username:
   print("Invalid username")
   login=input("Enter your username: ").lower().strip()
 print("login successful")
 print("quiz start")
+
+#login successful, quiz begin and record the score
 score=0
+
+#creat a dictionary involve five question, use for loop to print question and user answer question in order.
 Dictionary = {"Gerry Austin steals a wallet and uses the cash to rent a Mini. Which is the colour of MIni?     A) white  B) yellow C) black D) red":"B",
               "Gerry and John pick up___,who is heading to Wanganui A Sue B Shirl  C Mulvaney  D YOU":"B",
               "Who helps the trios in wellington?   A Sue B Shirl  C Mulvaney D YOU":"C",
@@ -14,11 +23,16 @@ Dictionary = {"Gerry Austin steals a wallet and uses the cash to rent a Mini. Wh
 for x in Dictionary:
     print(x)
     answer=input("Your answer is  :").upper().strip()
+
+    #use while loop to check the answer, which the user input, is valid or invalid. If the answer is invalid, user will
+    #fall in a loop untill they input a valid answer.
     validate_answers=['A','B','C','D']
     while answer  not in validate_answers:
         print("invalid answer, answer should in A B C D")
         answer=input("your answer is :").upper().strip()
         print(validate_answers)
+
+    #The answer is valid, then begin to check the answer. If right, score +1.. If wrong, print the right answer.
     else:
         print("your answer is"+ answer)
         if answer==Dictionary[x]:
@@ -27,6 +41,8 @@ for x in Dictionary:
             print("your score is: ",score)
         else:
             print("Incorrect,the answer is",Dictionary[x])
+
+#According to the score print different result to console
 if score==3 or score==4 :
     print("well done!but still need learn about it.")
 if score==1 or score==2:
